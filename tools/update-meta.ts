aada81a7b5caedcc9b4b74477abb002549a77373
Jayson Harshbarger <hypercubed@gmail.com>
2018-08-14 18:13:13 -0600

    Publish scripts

---
+++ tools/update-meta.ts
@@ -0,0 +1,40 @@
+/* The following code is from https://github.com/ngxs/store/blob/master/tools/set-metadata.ts */
+
+import { writeFile } from 'fs';
+import { getProjects } from './utils';
+const pkg = require('../package.json');
+
+async function main() {
+  const keysToCopy = [
+    'version',
+    'repository',
+    'keywords',
+    'author',
+    'contributors',
+    'license',
+    'bugs',
+    'homepage'
+  ];
+
+  const packages = getProjects();
+  for (const pack of packages) {
+    const packPath = `${pack.path}/package.json`;
+    const packPackage = require(packPath);
+
+    // copy all meta data from the root package.json into all packages
+    for (const key of keysToCopy) {
+      packPackage[key] = pkg[key];
+    }
+
+    // save the package file after we have updated the keys and peerDependencies
+    await writeFile(packPath, JSON.stringify(packPackage, null, 2), err => {
+      if (err) {
+        console.error('Write failed!');
+      }
+    });
+  }
+
+  console.log(`package version set to ${pkg.version}`);
+}
+
+main();
