6a8b540eb998e0612890fcb7634dbae25d8d9f7c
Jayson Harshbarger <jayson.harshbarger@swimlane.com>
2019-12-27 11:54:23 -0700

    Update integration tests

--- cypress/integration/main-nav_spec.js
+++ cypress/integration/main-nav_spec.js
@@ -13,31 +13,31 @@ describe('Main navigation', () => {
   });
 
   it('to features page', () => {
-    cy.get('@sidebar').find('md-toc[path="features"] a').first().click({ force: true });
+    cy.get('@sidebar').find('md-toc[src="/features"] a').first().click({ force: true });
     cy.url().should('include', '/features');
     cy.get('section.content').find('h1[id="content-features"]').contains('Content Features');
   });
 
   it('to content page', () => {
-    cy.get('@sidebar').find('md-toc[path="content"] a').first().click({ force: true });
+    cy.get('@sidebar').find('md-toc[src="/content"] a').first().click({ force: true });
     cy.url().should('include', '/content');
     cy.get('section.content').find('h1[id="content"]').contains('Content');
   });
 
   it('to customization page', () => {
-    cy.get('@sidebar').find('md-toc[path="modules"] a').first().click({ force: true });
+    cy.get('@sidebar').find('md-toc[src="/modules"] a').first().click({ force: true });
     cy.url().should('include', '/modules');
     cy.get('section.content').find('h1[id="modules"]').contains('Modules');
   });
 
   it('to quickstart page', () => {
-    cy.get('@sidebar').find('md-toc[path="quickstart"] a').first().click({ force: true });
+    cy.get('@sidebar').find('md-toc[src="/quickstart"] a').first().click({ force: true });
     cy.url().should('include', '/quickstart');
     cy.get('section.content').find('h1[id="quick-start"]').contains('Quick start');
   });
 
   it('content links', () => {
-    cy.get('@sidebar').find('md-toc[path="/"] a').first().click({ force: true });
+    cy.get('@sidebar').find('md-toc[src="/"] a').first().click({ force: true });
     cy.url().should('eq', 'http://localhost:4200/#docspa');
     cy.get('section.content').find('h1').contains('DocSPA');
     cy.get('section.content').find('a[href="/quickstart"]').first().click({ force: true });
